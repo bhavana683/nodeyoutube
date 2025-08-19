a7ed46e75d62cdb91092713061a54796dcfea5dc
bhavana683 <bhavanarupenaguntla@gmail.com>
2025-08-19 10:11:34 +0530

    LOGIN ROUTE 9

--- project/eep/src/SignUp.js
+++ project/eep/src/SignUp.js
@@ -261,9 +261,9 @@ const SignUp = () => {
         },
         withCredentials: true
       });
-      if(res.status===200||res.status===500){
-      navigate("SignIn");
-      }
+      
+      navigate("/SignIn");
+      
     } catch (err) {
       setErrorMessage(
         err.response?.data?.message || 
